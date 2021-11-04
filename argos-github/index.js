34d47425ffa3346011ffbaad235b0dadcdfc5a2c
Tristan264 <tristan.bschwenk@icloud.com>
2021-11-04 12:06:59 +0100

    feat (gitPull): micro-service & shell

--- argos-github/index.js
+++ argos-github/index.js
@@ -1,7 +1,11 @@
 import Koa from 'koa'
 import localtunnel from 'localtunnel';
+import bodyParser from 'koa-bodyparser';
+import { gitPull } from '../actions/gitPull.js';
 
+const wait = (delay) => new Promise(resolve => setTimeout(resolve, delay));
 (async () => {
+    await wait(500);
     const tunnel = await localtunnel({
         port: 8003,
         subdomain: "ezyo-server-manager"
@@ -16,16 +20,29 @@ import localtunnel from 'localtunnel';
         // tunnels are closed
         console.log("closing tunnel")
     });
+
 })();
 
 const app = new Koa();
 
-app.use(async ctx => {
-    const response = ctx.req
-    const webhook = await response.json()
+app.use(bodyParser());
 
+app.use(async ctx => {
+    const response = ctx.request.body
     console.log("=======NEW WEBHOOK=======")
-    console.log(webhook)
+
+    //Deconstruct object
+    const push = {
+        branch: response.ref.slice(11),
+        repository_name: response.repository.name,
+        repository_url: response.repository.url,
+        commits: response.commits.map((commit) => commit.message)
+    }
+
+    console.log(push)
+    ctx.status=200
+
+    gitPull(push)
 });
 
 app.listen(8003, () => {
