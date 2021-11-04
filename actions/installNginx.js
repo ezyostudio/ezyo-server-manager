37debfa522b0e68e3d7a9c2a2936f78a59b30dd2
Zao Soula <contact@zaosoula.fr>
2021-11-04 10:20:16 +0100

    .

--- actions/installNginx.js
+++ actions/installNginx.js
@@ -6,19 +6,22 @@ export default {
     description: 'Install NGINX if needed',
     value: 'installNginx',
     execute: async() => {
-
         if (shell.which('nginx')) {
             success('Nginx is already installed');
             return;
         }
 
-        const { stdout, stderr, code } = exec('apt install nginx');
+        const { stdout, stderr, code } = exec('apt-get install nginx');
 
         if (code == 0) {
             success('Nginx is now installed');
         } else {
-            fail('An error occured: ', stderr.trim());
+            const err = stderr.trim();
+            if(err.match(/are you root\?/)) {
+                return fail('You must be root to install Nginx, relaunch the tool as sudo or run "sudo apt-get install nginx"');
+            }
+            
+            return fail('An error occured: ', stderr.trim());
         }
-
     }
 };
