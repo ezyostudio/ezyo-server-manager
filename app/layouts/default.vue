7968f2f62ccee09049a835375769238f5605ef6b
Zao Soula <contact@zaosoula.fr>
2021-11-11 16:01:37 +0100

    feat(app): layout with navbar

---
+++ app/layouts/default.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="layout">
+    <Sidebar />
+    <Nuxt/>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .layout {
+    display: grid;
+    grid-template-columns: 100px 1fr;
+  }
+</style>
