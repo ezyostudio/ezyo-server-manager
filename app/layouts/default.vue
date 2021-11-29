1ebc6f21f94ba1dad4dd5f38dd9d7a55a7b8c995
Tristan264 <tristan.bschwenk@icloud.com>
2021-11-29 21:12:01 +0100

    fix(app) delete layout

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="layout">
-    <Navbar />
     <Nuxt/>
   </div>
 </template>
 
 <style lang="scss" scoped>
   .layout {
-    display: grid;
-    grid-template-columns: 100px 1fr;
     background-color: #f5f5fd;
     min-height: 100vh;
-    max-width: 100vw;
+    max-width: 100vw; 
   }
+
+</style>
+
+
+<style lang="scss">
+
 </style>
