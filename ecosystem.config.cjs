8cc028ed022b4cbf7b2503b0b58433381e3c2b7f
Zao Soula <contact@zaosoula.fr>
2021-11-29 19:41:42 +0100

    chore: improve launch scripts

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -1,8 +1,10 @@
 module.exports = {
   apps : [{
     name   : "EzyoServerManager-API",
-    script: 'index.js',
-    cwd: './api'
+    script: './api/index.js',
+  },{
+    name   : "EzyoServerManager-WEBHOOK",
+    script: './api/github.js',
   },{
     name: 'EzyoServerManager-APP',
     script: './node_modules/nuxt/bin/nuxt.js',
