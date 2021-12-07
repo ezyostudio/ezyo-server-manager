95954163fea7fbbbfbaf30a0c68bdd3d26c2caab
Zao Soula <contact@zaosoula.fr>
2021-12-07 19:57:27 +0100

    feat: add install nodejs action

--- actions/index.js
+++ actions/index.js
@@ -1,11 +1,13 @@
 import quit from './quit.js';
 import installNginx from './installNginx.js';
+import installNodejs from './installNodejs.js';
 import publishStatic from './publishStatic.js';
 import publishNodeJS from "./publishNodeJS.js";
 
 export default [
     quit,
     installNginx,
+    installNodejs,
     publishStatic,
     publishNodeJS
 ]
